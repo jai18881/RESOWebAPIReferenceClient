dc324953ad9bbbd64c8e14a5ef5b592ac2c9147b
Stuart Schuessler <stuarts50@gmail.com>
2017-02-28 17:17:35 -0500

    Add Library for a Rules Engine

---
+++ RESORuleEngine/IVerifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESORuleEngine
+{
+    public interface IVerifier
+    {
+        /// <summary>
+        /// Checks whether the constraint on the current request session is satisfied or not
+        /// </summary>
+        /// <param name="context">context object representing the current OData interop session</param>
+        /// <param name="result">output paramater to return detailed information if checking did not pass</param>
+        /// <returns>true/false</returns>
+        [SuppressMessage("Microsoft.Design", "CA1021:AvoidOutParameters", Justification = "followed the interface definition")]
+        bool Verify(ServiceContext context, out TestResult result);
+    }
+}
---
+++ RESORuleEngine/ServiceVerification.cs
@@ -0,0 +1,50 @@
+﻿namespace RESORuleEngine
+{
+    public class ServiceVerification
+    {
+        /// <summary>
+        /// Asc Sort Verification Result
+        /// </summary>
+        public ServiceVerificationResult AscSortVerResult = null;
+
+        /// <summary>
+        /// Desc Sort Verification Result
+        /// </summary>
+        public ServiceVerificationResult DescSortVerResult = null;
+
+        /// <summary>
+        /// Search Verification Result
+        /// </summary>
+        public ServiceVerificationResult SearchVerResult = null;
+
+        /// <summary>
+        /// Cross Join Verification Result
+        /// </summary>
+        public ServiceVerificationResult CrossJoinVerResult = null;
+
+        /// <summary>
+        /// Select Verification Result
+        /// </summary>
+        public ServiceVerificationResult SelectResult = null;
+
+        /// <summary>
+        /// Expand Verification Result
+        /// </summary>
+        public ServiceVerificationResult ExpandResult = null;
+
+        /// <summary>
+        /// Top Verification Result
+        /// </summary>
+        public ServiceVerificationResult TopResult = null;
+
+        /// <summary>
+        /// Count Verification Result
+        /// </summary>
+        public ServiceVerificationResult CountResult = null;
+
+        /// <summary>
+        /// Skip Verification Result
+        /// </summary>
+        public ServiceVerificationResult SkipResult = null;
+    }
+}
